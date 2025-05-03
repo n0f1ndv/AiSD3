70cf68e2632f9eea75057afb8ab847500631bb90
gruby_n0fin <93051703+n0f1ndv@users.noreply.github.com>
2025-05-03 13:46:01 +0200

    added creating, printing, finding to adj list

--- src/adj_lst.py
+++ src/adj_lst.py
@@ -1,14 +1,42 @@
+from collections import defaultdict
+
 def generate_adjacency_list():
     pass
 
-def input_adjacency_list():
-    pass
+def input_adjacency_list(nodes):
+    adjacency_list = defaultdict(list)
 
-def print_adjacency_list():
-    pass
+    for u in range(nodes):
+        vertices = [int(x) for x in input(f'{u}> ').split()]
+        
+        for v in vertices:
+            adjacency_list[u].append(v)
 
-def adjacency_list_find():
-    pass
+    # for debugging
+    print(adjacency_list)
+
+    return adjacency_list
+
+def print_adjacency_list(graph):
+    for u, v in graph.items():
+        print(f'{u}-> {v}')
+
+def adjacency_list_find(graph, start, end):
+    exists = False
+
+    print(graph[start])
+    for v in graph[start]:
+        print(v)
+        if v == end:
+            exists = True
+            break
+
+    if exists == True:
+        print(f'True: Edge {(start, end)} exists in the Graph.')
+    else:
+        print(f'False: Edge {(start, end)} doesn\'t exist in the Graph.')
+            
+
 
 def adjacency_list_bfs():
     pass
