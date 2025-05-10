eb9f3d8689a0ae1fbcf9a3c5d0a7c8c1a06ec039
gruby_n0fin <93051703+n0f1ndv@users.noreply.github.com>
2025-05-10 23:30:06 +0200

    backend was in wrong folder

---
+++ src/backend.py
@@ -0,0 +1,23 @@
+import sys
+
+def get_neighbors_to_insert(vertex, nodes):
+    vertices = []
+
+    while True:
+        try:
+            vertices = [int(x) for x in input(f'{vertex}> ').replace(',',' ').split()]
+            if any(j < 0 for j in vertices):
+                print('Error: Nodes\' labels MUST be greater than zero.')
+                continue
+            if any(j > nodes for j in vertices):
+                print('Error: Nodes\' labels MUST NOT exceed the defined number of nodes.')
+                continue
+            break
+
+        except ValueError:
+            print('Node MUST be an integer.')
+        except KeyboardInterrupt:
+            print('\nKeyboard Interrupt')
+            sys.exit(1)
+
+    return vertices
